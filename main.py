efc982ecf4922592b987cf7f6cad7cec886b4a1d
Back777space <ivan.molenaers@hotmail.com>
2022-07-17 14:59:15 +0200

    new quote daily

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import json
-import random
 import os
+from datetime import date
 
-currDir = os.path.dirname(os.path.realpath(__file__))
+CURRDIR = os.path.dirname(os.path.realpath(__file__))
+YELLOW = (194,217,76)
+PINKISH = (248,131,121)
 
 def loadQuotes():
-    with open (f'{currDir}/quotes.json', "r") as f:
+    with open (f'{CURRDIR}/quotes.json') as f:
         # Reading from file
-        data = json.loads(f.read())
+        data = json.load(f)
         return data
 
 def to_coloured(col):
@@ -15,9 +17,10 @@ def to_coloured(col):
 
 quotes = loadQuotes().get("quotes")
 
-randomQuote = quotes[random.randrange(0, len(quotes))]
+curr_date = date.today()
+randomQuote = quotes[(curr_date.day + curr_date.month + curr_date.year) % len(quotes)]
 
 bold = "\033[1m"
 
-print(bold + to_coloured((194,217,76)) + randomQuote.get("text") + \
-        "\n - " + to_coloured((248,131,121)) + randomQuote.get("author"))
+print(bold + to_coloured(YELLOW) + randomQuote.get("text") + \
+        "\n- " + to_coloured(PINKISH) + randomQuote.get("author"))
